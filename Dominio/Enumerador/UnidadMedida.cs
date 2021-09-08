67a2784d4c0c84e3b77ef05f2dc4b4380f5e2d27
josebaltodano <89146748+josebaltodano@users.noreply.github.com>
2021-09-08 12:00:55 -0600

    Agregar CRUD, Consultas, Metodo Privadp

---
+++ Dominio/Enumerador/UnidadMedida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Dominio.Enumerador
+{
+    public enum UnidadMedida
+    {
+        Unidades,
+        Litros,
+        Mililitro,
+        Kilogramo,
+        Gramos,
+        Libras
+    }
+}
---
+++ Infraestructura/Productos/ProductoModelo.cs
@@ -0,0 +1,125 @@
+﻿#region Usos
+using System;
+using System.Text;
+using Dominio.Entidades;
+using Dominio.Enumerador;
+using System.Collections.Generic;
+#endregion
+#region Infraestructura
+namespace Infraestructura.Productos
+{
+    public class ProductoModelo
+    {
+        private Producto[] productos;
+        #region CRUD
+        public void Añadir(Producto p)
+        {
+            if (p == null)
+            {
+                throw new ArgumentException("El Producto no puede ser null");
+            }
+            Añadir(p, ref productos);
+        }
+        public int Actualizar(Producto p)
+        {
+            if (p == null)
+            {
+                throw new ArgumentException("El Producto no puede ser null");
+            }
+            int indice = ObtenerIndiceId(p.Id);
+            if (indice < 0)
+            {
+                throw new ArgumentException($"El Producto con Id:{p.Id} no exixte");
+            }
+            productos[indice] = p;
+            return indice;
+        }
+        public bool Eliminar(Producto p)
+        {
+            if (p == null)
+            {
+                throw new ArgumentException("El Producto no puede ser null");
+            }
+            int indice = ObtenerIndiceId(p.Id);
+            if (indice < 0)
+            {
+                throw new ArgumentException($"El Producto con Id:{p.Id} no exixte");
+            }
+            if (indice != (productos.Length - 1))
+            {
+                productos[indice] = productos[productos.Length - 1];
+            }
+            Producto[] tmp = new Producto[productos.Length - 1];
+            Array.Copy(productos, tmp, tmp.Length);
+            productos = tmp;
+
+            return productos.Length == tmp.Length;
+        }
+        public Producto[] ObtenerTodo()
+        {
+            return productos;
+        }
+        #endregion
+        #region Consultas
+        public Producto ObtenerProductoId(int id)
+        {
+            int indice = ObtenerIndiceId(id);
+            return indice < 0 ? null :productos[indice];
+        }
+        public Producto[] ObtenerProductoUnidadMedida(UnidadMedida um)
+        {
+            Producto[] tmp = null;
+            if (productos == null)
+            {
+                return tmp;
+            }
+            foreach (Producto p in productos)
+            {
+                if (p.UnidadMedida == um)
+                {
+                    Añadir(p, ref tmp);
+                }
+            }
+            return tmp;
+        }
+        #endregion
+        #region Metodo Privado
+        private void Añadir(Producto p, ref Producto[] pds)
+        {
+            if (pds == null)
+            {
+                pds = new Producto[1];
+                pds[pds.Length - 1] = p;
+                return;
+            }
+            Producto[] tmp = new Producto[pds.Length + 1];
+            Array.Copy(pds, tmp, pds.Length);
+            tmp[tmp.Length - 1] = p;
+            pds = tmp;
+        }
+        private int ObtenerIndiceId(int id)
+        {
+            int indice = int.MinValue, i = 0;
+            if (id <= 0)
+            {
+                throw new ArgumentException($"El Id:{id} no puede ser menor o igual que cero");
+            }
+            if (productos == null)
+            {
+                return indice;
+            }
+            foreach (Producto p in productos)
+            {
+                if (p.Id == id)
+                {
+                    indice = i;
+                    break;
+                }
+                i++;
+            }
+            return indice;
+        }
+        #endregion
+    }
+}
+#endregion
